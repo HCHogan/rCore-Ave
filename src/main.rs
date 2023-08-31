14b3b2a863c768d2f4dc8b255fb2674aca58dcef
Hank Hogan <ysh2291939848@outlook.com>
2023-08-31 18:34:11 +0800

    init

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+
+mod lang_items;
+mod sbi;
+
+use core::arch::global_asm;
+global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    loop {}
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+
+    (sbss as usize..ebss as usize).for_each(|addr| {
+        unsafe {
+            (addr as *mut u8).write_volatile(0);
+        }
+    });
+}
